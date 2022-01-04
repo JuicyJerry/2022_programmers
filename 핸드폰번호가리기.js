71057c4b982953ae3d26688315c8fff5cd836578
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-01-05 00:03:16 +0900

    programmers #2

---
+++ 핸드폰번호가리기.js
@@ -0,0 +1,15 @@
+function solution(phone_number) {
+    var answer = '';
+    
+    let len = phone_number.length;
+    
+    for (let i = 0; i < len; i++) {
+        if (i >= len - 4) {
+            answer += phone_number[i];
+        } else {
+            answer += '*';
+        }
+    }
+    
+    return answer;
+}
