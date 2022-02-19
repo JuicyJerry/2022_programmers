a8d96d368aca28e7c30df155beddaa2cb27ebb4a
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2022-02-20 02:22:13 +0900

    today algo

--- 음양더하기.js
+++ 음양더하기.js
@@ -1,9 +1,24 @@
-function solution(absolutes, signs) {
-    let answer = 0;
-    
-    signs.map((el, idx) => {
-        if (el) answer += absolutes[idx];
-        else answer -= absolutes[idx];
-    })    
-    return answer;
+function solution(abs, signs) {
+    let total = 0;
+    let sum = abs.filter((el, idx) => {
+        if (signs[idx] === true) total += el;
+        else total -= el;
+    })
+    // console.log(total, sum);
+    return total;
 }
+
+function solution(abs, signs) {
+    return abs.reduce((acc, cur, idx) => signs[idx] === true ? acc + cur : acc - cur , 0);
+    
+}
+
+// function solution(absolutes, signs) {
+//     let answer = 0;
+    
+//     signs.map((el, idx) => {
+//         if (el) answer += absolutes[idx];
+//         else answer -= absolutes[idx];
+//     })    
+//     return answer;
+// }
